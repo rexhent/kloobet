b966d994e9243503979120a98ae0cc8648e23dc5
rexhent <dhannah10@outlook.com>
2023-11-16 20:20:51 +1100

    first

--- app/page.tsx
+++ app/page.tsx
@@ -13,38 +13,9 @@ export default function Home() {
           className="w-48 h-48"
         />
         <div className="text-center max-w-screen-sm mb-10">
-          <h1 className="text-stone-200 font-bold text-2xl">
-            Next.js Prisma PostgreSQL Auth Starter
-          </h1>
+          <h1 className="text-stone-200 font-bold text-2xl">Kloobet</h1>
           <p className="text-stone-400 mt-5">
-            This is a{" "}
-            <a
-              href="https://nextjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-stone-400 underline hover:text-stone-200 transition-all"
-            >
-              Next.js
-            </a>{" "}
-            starter kit that uses{" "}
-            <a
-              href="https://next-auth.js.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-stone-400 underline hover:text-stone-200 transition-all"
-            >
-              Next-Auth
-            </a>{" "}
-            for simple email + password login and a{" "}
-            <a
-              href="https://vercel.com/postgres"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-stone-400 underline hover:text-stone-200 transition-all"
-            >
-              Vercel Postgres
-            </a>{" "}
-            database to persist the data.
+            Clone of Blooket made in NextJS, and React.
           </p>
         </div>
         <div className="flex space-x-3">
@@ -53,25 +24,25 @@ export default function Home() {
             prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
             className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
-            Protected Page
+            Login
           </Link>
           <p className="text-white">·</p>
           <a
-            href="https://github.com/steven-tey/nextjs-typescript-starter"
+            href="https://github.com/rexhent/kloobet/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
-            GitHub
+            Source Code
           </a>
           <p className="text-white">·</p>
           <a
-            href="https://vercel.com/templates/next.js/prisma-postgres-auth-starter"
+            href="https://github.com/rexhent/kloobet/issues"
             target="_blank"
             rel="noopener noreferrer"
             className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
-            1-click Deploy to Vercel
+            To-Do
           </a>
         </div>
       </div>
